d5cb0310ed4408bcd11ef086887c3d5d0be0efcc
Benjamin Brandau <codingbrandau@gmail.com>
2020-07-30 19:09:51 -0600

    Got CRUD

---
+++ HogWartsHousesAustin/Controllers/model Controller/houseGuessControler.swift
@@ -0,0 +1,9 @@
+//
+//  houseGuessControler.swift
+//  HogWartsHousesAustin
+//
+//  Created by Ben Brandau Brandau on 7/30/20.
+//  Copyright © 2020 Ben Brandau. All rights reserved.
+//
+
+import Foundation
